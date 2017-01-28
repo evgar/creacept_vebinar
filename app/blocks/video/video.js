1606ce885af64d722cdb991a16073e1d65baa164
evgar <eugene.garichev@gmail.com>
2017-01-28 19:33:30 +0200

    add form-registration block

--- app/blocks/video/video.js
+++ app/blocks/video/video.js
@@ -4,22 +4,24 @@ tag.src = "//www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+
 setTimeout(function onYouTubePlayerAPIReady() {
 	player = new YT.Player('video', {
 		events: {
 			'onReady': onPlayerReady
 		}
 	});
-},1000);
+}, 2000);
 
-	var player;
+var player;
 
-	function onPlayerReady() {
-		var playButton = document.getElementsByClassName("video__play-button")[0];
-		playButton.addEventListener("click", function () {
-			playButton.style.display="none";
-			player.playVideo();
-		});
-	}
+
+function onPlayerReady() {
+	var playButton = document.getElementsByClassName("video__play-button")[0];
+	playButton.addEventListener("click", function () {
+		playButton.style.display = "none";
+		player.playVideo();
+	});
+}
 
 
